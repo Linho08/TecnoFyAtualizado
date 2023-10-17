933ffbde39a0a5971d8c47c41f5db14b705d69ed
filipenrs <filipe.santos7@ba.estudante.senai.br>
2023-10-16 21:32:48 -0300

    formulario.update

--- consulta.js
+++ consulta.js
@@ -1,7 +1,1 @@
-document.getElementById("button").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "flex";
-})
-document.getElementsByClassName(".close").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
-})
 
