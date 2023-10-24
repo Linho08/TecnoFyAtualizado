bb1be83b892d2c08cf96a36cfb77c59c4bf00be5
Linho08 <paulosenai6174@gmail.com>
2023-10-24 11:38:47 -0300

    Atualização 24/10 11:40

---
+++ teste.js
@@ -0,0 +1,75 @@
+AOS.init();
+
+// INICIO BODY
+
+function carregou() {
+    console.log('Página carregada com sucesso')
+}
+
+// FINAL BODY
+
+
+// INICIO HEADER (CABECALHO)
+function logo() {
+    console.log('Foco na Logo')
+}
+
+// FINAL HEADER
+
+
+// INICIO ARTICLE (CORPO)
+
+/* Carrossel */
+(function () {
+    "use strict";
+
+    var carousels = function () {
+        $(".owl-carousel1").owlCarousel({
+            loop: true,
+            center: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
+                },
+                1000: {
+                    items: 2,
+                    nav: false,
+                    loop: false
+                },
+                1000: {
+                    items: 3,
+                    nav: true
+                }
+            }
+        });
+    };
+
+    (function ($) {
+        carousels();
+    })(jQuery);
+})
+/* FINAL CARROSSEL */
+
+
+// FINAL ARTICLE
+
+
+// INICIO FOOTER (RODAPE)
+
+/* TECNOFY transicao */
+const buttonsComponent = document.querySelector('.buttons');
+const buttonsToggle = document.querySelector('.buttons__toggle');
+
+buttonsToggle.addEventListener('click', toggleButtons);
+
+function toggleButtons() {
+    buttonsToggle.classList.toggle('buttons__toggle--active');
+    buttonsComponent.classList.toggle('buttons--active');
+    document.activeElement.blur();
+}
+
+// FINAL FOOTER
